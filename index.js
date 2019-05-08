649a43b35af0f0092e483b602fad683c6ea39a8d
Erin Dustin <erincdustin@gmail.com>
2019-05-07 22:22:54 -0500

    added displayResults function

--- index.js
+++ index.js
@@ -10,13 +10,28 @@ const store = {
   formReady: false,
 };
 
+function displayResults(responseJson, maxNumber) {
+  console.log(responseJson);
+  $('.results-list').empty();
+  for (let i = 0; i < responseJson.data.length & i < maxNumber; i++) {
+    $('.results-list').append(
+      `<li>
+        <h3><a href= "${responseJson.data[i].url}">${responseJson.data[i].name}</a></h3>
+        <p>${responseJson.data[i].description}</p>
+      </li>`
+    );
+  }
+}
+$('.results').removeClass('hidden');
+
+
 function formatQueryParams(params) {
   const queryItems= Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
-function requestPark(query, maxNum) {
+function requestPark(query, maxNumber) {
   console.log('Request park with query and limit it to maxNum');
   const params = {
     q: query,
@@ -24,12 +39,12 @@ function requestPark(query, maxNum) {
   const queryString = formatQueryParams(params);
   const url = searchURL + '&' + queryString;
 
-  //console.log(url);
+  console.log(url);
 
-  const options = {
-    headers: new Headers({
-      "X-Api-Key": apiKey})
-  };
+  // const options = {
+  //   headers: new Headers({
+  //     "X-Api-Key": apiKey})
+  // };
 
   fetch(url)
     .then(response => {
@@ -38,19 +53,19 @@ function requestPark(query, maxNum) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => displayResults(responseJson, maxNumber))
     .catch(e => {
-      console.log(e);
+      $('.error-message').text(`Something went wrong: ${e.message}`);
     });
 }
 
 function watchForm() {
-  //console.log('Set event listener here');
   $('#park-search').on('submit', event => {
     event.preventDefault();
     const searchTerm = event.target.querySearch.value;
     const maxResults = event.target.maxNum.value;
     requestPark(searchTerm, maxResults);
+    console.log(`${searchTerm} ${maxResults}`);
   });
 }
 
